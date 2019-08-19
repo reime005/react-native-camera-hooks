26cf18b86674aa4e8479320a7f5e70b1c269cd13
Marius Reimer <reime005@gmail.com>
2019-08-19 21:44:59 +0200

    expo example upgrade

--- example/ExpoExample/App.tsx
+++ example/ExpoExample/App.tsx
@@ -8,13 +8,13 @@ import { useCamera } from 'react-native-camera-hooks';
 export default function App() {
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
 
-  const [{ cameraRef, type }, { toggleFacing }] = useCamera({  });
+  const [{ cameraRef, type }, { toggleFacing, recordVideo }] = useCamera();
 
   useEffect(() => {
     const checkPerm = async () => {
       const { status } = await Permissions.askAsync(Permissions.CAMERA);
       setHasCameraPermission(status === 'granted');
-    }
+    };
 
     checkPerm();
   }, [setHasCameraPermission]);
@@ -32,15 +32,38 @@ export default function App() {
               flex: 1,
               backgroundColor: 'transparent',
               flexDirection: 'row',
-            }}>
+            }}
+          >
             <TouchableOpacity
               style={{
                 flex: 0.1,
                 alignSelf: 'flex-end',
                 alignItems: 'center',
               }}
-              onPress={toggleFacing}>
-              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+              onPress={toggleFacing}
+            >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                Flip
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 0.2,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+              }}
+              onPress={async () => {
+                try {
+                  const data = await recordVideo();
+                  console.warn(data);
+                } catch (e) {
+                  console.warn(e);
+                }
+              }}
+            >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                Record
+              </Text>
             </TouchableOpacity>
           </View>
         </Camera>
@@ -48,4 +71,3 @@ export default function App() {
     );
   }
 }
-
